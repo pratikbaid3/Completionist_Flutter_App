37913c40eaf4438e5ecdf9bc154b2117b6f11518
pratikbaid3 <pratikbaid3@gmail.com>
2020-12-14 23:06:50 +0530

    feat: Completed list in guide page

--- lib/Pages/guide_page.dart
+++ lib/Pages/guide_page.dart
@@ -1,6 +1,8 @@
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:expansion_tile_card/expansion_tile_card.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:flutter_widget_from_html/flutter_widget_from_html.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:game_trophy_manager/Provider/game_provider.dart';
 import 'package:game_trophy_manager/Provider/guide_provider.dart';
@@ -17,6 +19,7 @@ class GuidePage extends StatefulWidget {
 }
 
 class _GuidePageState extends State<GuidePage> {
+  bool isExpanded = false;
   @override
   Widget build(BuildContext context) {
     double hp = MediaQuery.of(context).size.height;
@@ -26,151 +29,137 @@ class _GuidePageState extends State<GuidePage> {
         appBar: AppBar(),
       ),
       body: Padding(
-          padding:
-              EdgeInsets.symmetric(horizontal: wp * 0.03, vertical: hp * 0.03),
-          child: Column(
-            children: [
-              Expanded(
-                child: FutureBuilder(
-                  future: Provider.of<GuideProvider>(context)
-                      .getGuide(gameName: widget.game),
-                  builder: (BuildContext context, AsyncSnapshot snapshot) {
-                    if (Provider.of<GuideProvider>(context).guide.length == 0) {
-                      return Container(
-                        height: hp * 0.6,
-                        child: Center(
-                          child: SpinKitFadingCircle(
-                            color: primaryAccentColor,
-                            size: 60.0,
+        padding:
+            EdgeInsets.symmetric(horizontal: wp * 0.03, vertical: hp * 0.03),
+        child: Column(
+          children: [
+            Expanded(
+              child: FutureBuilder(
+                future: Provider.of<GuideProvider>(context)
+                    .getGuide(gameName: widget.game),
+                builder: (BuildContext context, AsyncSnapshot snapshot) {
+                  if (Provider.of<GuideProvider>(context).guide.length == 0) {
+                    return Container(
+                      height: hp * 0.6,
+                      child: Center(
+                        child: SpinKitFadingCircle(
+                          color: primaryAccentColor,
+                          size: 60.0,
+                        ),
+                      ),
+                    );
+                  }
+                  return ListView.builder(
+                    primary: false,
+                    shrinkWrap: true,
+                    padding: EdgeInsets.only(top: 10, left: 2, right: 2),
+                    itemCount: Provider.of<GuideProvider>(context).guide.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return ExpansionTileCard(
+                        initialPadding: EdgeInsets.symmetric(vertical: 10),
+                        borderRadius: BorderRadius.circular(10),
+                        initialElevation: 0,
+                        elevation: 0,
+                        baseColor: secondaryColor,
+                        expandedColor: secondaryColor,
+                        onExpansionChanged: (value) {
+                          setState(() {
+                            isExpanded = value;
+                          });
+                        },
+                        trailing: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Icon(
+                              FontAwesomeIcons.trophy,
+                              size: 15,
+                              color: (Provider.of<GuideProvider>(context)
+                                          .guide[index]
+                                          .trophyType ==
+                                      'BRONZE')
+                                  ? bronzeColor
+                                  : ((Provider.of<GuideProvider>(context)
+                                              .guide[index]
+                                              .trophyType ==
+                                          'SILVER')
+                                      ? silverColor
+                                      : goldenColor),
+                            ),
+                            (!isExpanded)
+                                ? Icon(
+                                    Icons.keyboard_arrow_down_rounded,
+                                    color: Colors.white,
+                                  )
+                                : Icon(
+                                    Icons.keyboard_arrow_up_rounded,
+                                    color: Colors.white,
+                                  ),
+                          ],
+                        ),
+                        title: ListTile(
+                          contentPadding: EdgeInsets.symmetric(vertical: 13.0),
+                          leading: Container(
+                              padding: EdgeInsets.only(right: 12.0),
+                              decoration: new BoxDecoration(
+                                  border: new Border(
+                                      right: new BorderSide(
+                                          width: 1.0, color: Colors.white24))),
+                              child: Hero(
+                                tag:
+                                    '${Provider.of<GuideProvider>(context).guide[index].trophyName}',
+                                child: CachedNetworkImage(
+                                  imageUrl: Provider.of<GuideProvider>(context)
+                                      .guide[index]
+                                      .trophyImage,
+                                  placeholder: (context, url) =>
+                                      new CircularProgressIndicator(
+                                    backgroundColor: primaryAccentColor,
+                                  ),
+                                  errorWidget: (context, url, error) =>
+                                      new Icon(Icons.error),
+                                ),
+                              )),
+                          title: Text(
+                            '${Provider.of<GuideProvider>(context).guide[index].trophyName}',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          subtitle: Padding(
+                            padding: EdgeInsets.only(top: 8.0),
+                            child: Text(
+                              Provider.of<GuideProvider>(context)
+                                  .guide[index]
+                                  .trophyDescription,
+                              style: TextStyle(
+                                color: Colors.white60,
+                              ),
+                            ),
                           ),
                         ),
-                      );
-                    }
-                    return ListView.builder(
-                      primary: false,
-                      shrinkWrap: true,
-                      padding: EdgeInsets.only(top: 10),
-                      itemCount:
-                          Provider.of<GuideProvider>(context).guide.length,
-                      itemBuilder: (BuildContext context, int index) {
-                        return Card(
-                          color: Colors.transparent,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                          elevation: 8.0,
-                          margin: new EdgeInsets.symmetric(vertical: 10.0),
-                          child: Container(
+                        children: [
+                          Container(
                             decoration: BoxDecoration(
-                              color: secondaryColor,
-                              borderRadius: new BorderRadius.all(
-                                  const Radius.circular(10)),
+                              borderRadius: BorderRadius.circular(10),
                             ),
-                            child: ListTile(
-                              contentPadding: EdgeInsets.symmetric(
-                                  horizontal: 20.0, vertical: 13.0),
-                              leading: Container(
-                                  padding: EdgeInsets.only(right: 12.0),
-                                  decoration: new BoxDecoration(
-                                      border: new Border(
-                                          right: new BorderSide(
-                                              width: 1.0,
-                                              color: Colors.white24))),
-                                  child: Hero(
-                                    tag:
-                                        '${Provider.of<GuideProvider>(context).guide[index].trophyName}',
-                                    child: CachedNetworkImage(
-                                      imageUrl:
-                                          Provider.of<GuideProvider>(context)
-                                              .guide[index]
-                                              .trophyImage,
-                                      placeholder: (context, url) =>
-                                          new CircularProgressIndicator(
-                                        backgroundColor: primaryAccentColor,
-                                      ),
-                                      errorWidget: (context, url, error) =>
-                                          new Icon(Icons.error),
-                                    ),
-                                  )),
-                              title: Text(
-                                '${Provider.of<GuideProvider>(context).guide[index].trophyName}',
-                                style: TextStyle(
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.bold),
-                              ),
-                              trailing: Icon(Icons.keyboard_arrow_right,
-                                  color: Colors.white, size: 30.0),
-                              subtitle: Padding(
-                                padding: EdgeInsets.only(top: 10.0),
-                                child: Row(
-                                  children: [
-                                    Row(
-                                      children: <Widget>[
-                                        Icon(
-                                          FontAwesomeIcons.trophy,
-                                          color: goldenColor,
-                                          size: 12,
-                                        ),
-                                        Text(
-                                          "  3",
-                                          style: TextStyle(
-                                            color: Colors.white,
-                                            fontWeight: FontWeight.bold,
-                                          ),
-                                        )
-                                      ],
-                                    ),
-                                    SizedBox(
-                                      width: 10,
-                                    ),
-                                    Row(
-                                      children: <Widget>[
-                                        Icon(
-                                          FontAwesomeIcons.trophy,
-                                          color: silverColor,
-                                          size: 12,
-                                        ),
-                                        Text(
-                                          "  3",
-                                          style: TextStyle(
-                                            color: Colors.white,
-                                            fontWeight: FontWeight.bold,
-                                          ),
-                                        )
-                                      ],
-                                    ),
-                                    SizedBox(
-                                      width: 10,
-                                    ),
-                                    Row(
-                                      children: <Widget>[
-                                        Icon(
-                                          FontAwesomeIcons.trophy,
-                                          color: bronzeColor,
-                                          size: 12,
-                                        ),
-                                        Text(
-                                          "  3",
-                                          style: TextStyle(
-                                            color: Colors.white,
-                                            fontWeight: FontWeight.bold,
-                                          ),
-                                        )
-                                      ],
-                                    )
-                                  ],
-                                ),
-                              ),
+                            child: HtmlWidget(
+                              '''${Provider.of<GuideProvider>(context).guide[index].trophyGuide}''',
+                              textStyle: TextStyle(fontSize: 15),
+                              webView: true,
                             ),
-                          ),
-                        );
-                      },
-                    );
-                  },
-                ),
+                            padding: EdgeInsets.symmetric(
+                                horizontal: 30, vertical: 20),
+                          )
+                        ],
+                      );
+                    },
+                  );
+                },
               ),
-            ],
-          )),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
