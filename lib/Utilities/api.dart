fd95562645042259b3e9f00c75ff3a0ce8ab2732
Pratik Baid <pratikbaid3@gmail.com>
2021-07-19 12:46:36 +0530

    Update api.dart

--- lib/Utilities/api.dart
+++ lib/Utilities/api.dart
@@ -1,4 +1,3 @@
-// const String baseUrl = 'http://127.0.0.1:8000/api/';
-const String baseUrl = 'https://completionist-django.herokuapp.com/api/';
+const String baseUrl = 'http://127.0.0.1:8000/api/';
 const String gamesUrl = 'ps4/games';
 const String guideUrl = "ps4/guide/";
