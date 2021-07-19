6e5038ca5a5fb3516d8f22084ed79336ee001e3a
pratikbaid3 <pratikbaid3@gmail.com>
2021-07-19 12:43:28 +0530

    feat: Updated all the packages to null safety

--- lib/Utilities/api.dart
+++ lib/Utilities/api.dart
@@ -1,3 +1,4 @@
-const String baseUrl = 'http://127.0.0.1:8000/api/';
+// const String baseUrl = 'http://127.0.0.1:8000/api/';
+const String baseUrl = 'https://completionist-django.herokuapp.com/api/';
 const String gamesUrl = 'ps4/games';
 const String guideUrl = "ps4/guide/";
